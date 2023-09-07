a74186a5b9ac0cec615b950267dbe34cacee57db
manoharmaheshwar <66367845+manoharmaheahwar@users.noreply.github.com>
2023-09-07 12:59:09 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
         <div className='navlinks-container'>
             <ul className='links text-gray-100 font-mono'>
                 <li><Link to="/" className=' cursor-pointer'>Home</Link></li>
-                <li><Link to="/" className=' cursor-pointer'>About</Link></li>
+                <li><Link to="https://flowcv.com/resume/9ft2qfswid" className=' cursor-pointer'>About</Link></li>
                 <li onClick={()=>navigate("/contact")} className=' cursor-pointer'> Contact</li>
                 <li onClick={()=>navigate("/projects")} className=' cursor-pointer'>Projects</li>
             </ul>
